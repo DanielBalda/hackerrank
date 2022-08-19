32788cd58d56c14942f732f5c6d6ab05a3fa451e
danielbalda <danielbalda93@gmail.com>
2022-08-19 19:24:37 -0300

    challenge

--- 1 Week Preparation Kit/Day 2/Diagonal Difference.py
+++ 1 Week Preparation Kit/Day 2/Diagonal Difference.py
@@ -1,7 +1,12 @@
-arr = [[11, 2, 4], [4, 5, 6], [10, 8, -12]]
-count, ldiag, rdiag = 0, 0, 0
-for row in arr:
-    rdiag += row[count]
-    ldiag += row[len(arr)-1-count]
-    count += 1
+def diagonalDifference(arr):
+    count, ldiag, rdiag = 0, 0, 0
+    for row in arr:
+        rdiag += row[count]
+        ldiag += row[len(arr)-1-count]
+        count += 1
     return abs(ldiag-rdiag)
+
+
+if __name__ == '__main__':
+    diagonalDifference([[11, 2, 4], [4, 5, 6], [10, 8, -12]])
+    
